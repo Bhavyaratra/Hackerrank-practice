0bd005b89dbf2bc5e4d4397e247c14f90030187f
Bhavya Ratra <bhavya.r1220@gmail.com>
2020-09-29 03:41:13 +0530

    added beautiful triplet

---
+++ Algorithm/Implementation/Beautiful-triplets.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<utility>
+#include<set>
+#include<math.h>
+
+
+using namespace std;
+
+int main() 
+{
+    int n,d,count=0;
+    cin>>n>>d;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    for(int i=0;i<n;i++)
+    {   int flag=0;
+        int x=a[i]+d;
+        int y=a[i]+(d*2); //or y=x+d;
+        
+        for(int j=0;j<n;j++)
+        {
+            if(a[j]==x)
+            {
+                flag=flag+1;
+            }
+            else if(a[j]==y)
+            {
+                flag+=1;
+            }
+        }
+        if(flag!=2)
+        {
+            continue;
+        }
+        else
+        {
+            count++;
+        }
+        
+    }    
+    cout<<count;
+
+}    
