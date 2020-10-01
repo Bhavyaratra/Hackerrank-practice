5f205eb77316a1314ae94a06d44444ceb37b62ef
Bhavya Ratra <bhavya.r1220@gmail.com>
2020-10-01 20:30:58 +0530

    modified CamelCase.cpp

--- Algorithm/Strings/CamelCase.cpp
+++ Algorithm/Strings/CamelCase.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include<string>
+
 using namespace std;
 
 int main()
 { 
     string s;
+     cin>>s;
     int count=0;
     for(int i=0;i<s.size();i++)
     {
