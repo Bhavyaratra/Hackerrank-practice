2bd79596cc7251c4ee9539c54dba3a0e0736a29a
Bhavya Ratra <bhavya.r1220@gmail.com>
2020-10-03 03:41:05 +0530

    modified TwoCharacters.cpp (using pairs)

--- Algorithm/Strings/TwoCharacters.cpp
+++ Algorithm/Strings/TwoCharacters.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <set>
 #include<string>
+#include<map>
 
+#include<assert.h>
 using namespace std;
 
+int make_string(string str, char a, char b)
+{
+    int len = str.size();
+    string temp = "";
+    char last;
 
-
-int main()
-{ 
-    int n;
-    string s;
-    cin>>n;
-    cin>>s;
-    int arr[26]={0};
-    for(int i=0;i<n;i++)
-    {  
-        arr[s[i]-'a']++;
+    for(int i=0; i<len; i++)
+    {
+        if(str[i]== a || str[i] == b)
+            temp+=str[i];
     }
-    
-    
-}    
+
+    len = temp.size();
+    if(len <= 1)
+        return 0;
+
+    last = temp[0];
+    for(int i=1; i<len; i++)
+    {
+        if(temp[i] == last)
+            return 0;
+        last = temp[i];
+    }
+    return len;
+}
+
+int main(){
+string s; char a, b;
+cin>>s;
+cin>>a>>b;
+
+cout<<make_string(s, a, b);
+return 0;
+}
