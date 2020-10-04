3b62005ebad6490d85ee48d086a0c83db7550327
Bhavya Ratra <bhavya.r1220@gmail.com>
2020-10-04 15:04:43 +0530

    added Minimum height trriangle

---
+++ Algorithm/Mathematics/MinimumHeightTriangle.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b;
+    float h;
+    cin>>b>>a;
+    h=((2*a)+(b-1))/b;
+    cout<<h;
+    return 0;
+}
