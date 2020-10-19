8b6677c804f164fce34ca6eb130e2aafb21a2d15
Bhavya Ratra <bhavya.r1220@gmail.com>
2020-10-19 23:49:48 +0530

    added FairRation.cpp

---
+++ Algorithm/Implementation/FairRation.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n],c=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        if(arr[i]%2!=0)
+        {
+            c++;
+        }
+    }
+    if(c%2!=0)
+    {
+        cout<<"No";
+        return 0;
+    }
+    c=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            arr[i]=arr[i]+1;
+            arr[i+1]=arr[i+1]+1;
+            c=c+2;
+        }
+    }
+    cout<<c;
+    
+}    
