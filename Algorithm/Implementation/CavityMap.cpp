ecc44f394b5adf8648aa0f950e36edf84f20e855
Bhavya Ratra <bhavya.r1220@gmail.com>
2020-10-21 17:26:08 +0530

    updated CavityMap.cpp

--- Algorithm/Implementation/CavityMap.cpp
+++ Algorithm/Implementation/CavityMap.cpp
@@ -1,9 +1,42 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<unordered_map>
+#include<algorithm>
+
 
 using namespace std;
 
 int main()
 {
-    
+    unordered_map<int,string> v;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        string s;
+        cin>>s;
+        v[i]=s;
+    }
+    for(int i=1;i<n-1;i++)
+    {
+        for(int j=1;j<n-1;j++)
+        {
+            if(v[i][j]>v[i-1][j] && v[i][j]>v[i+1][j] && v[i][j]>v[i][j-1] && v[i][j]>v[i][j+1])
+            {
+                v[i][j]='X';
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            
+            {
+                cout<<v[i][j];
+            }
+        }
+        cout<<endl;
+    }
 }
