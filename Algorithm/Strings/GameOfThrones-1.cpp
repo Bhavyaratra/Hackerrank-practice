fb794720f65352dfc8f2702311eb3b8b7ca67c98
Bhavya Ratra <bhavya.r1220@gmail.com>
2020-10-29 14:37:46 +0530

    added less LOC GameofThrones-1.cpp

--- Algorithm/Strings/GameOfThrones-1.cpp
+++ Algorithm/Strings/GameOfThrones-1.cpp
@@ -58,3 +58,23 @@ int main()
     }
     
 }
+
+/*        compact solution
+int main(){
+  string s;
+  cin >>  s;
+
+  vector<int> v(256, 0);
+  for(int i=0; i<s.size(); i++) v[s[i]]++;
+
+  int impar =0;
+  for(int i=0; i<256; i++)
+    if(v[i]%2) impar++;
+
+  if((s.size()%2 == 0 && impar == 0) || (s.size()%2==1 && impar == 1))
+    puts("YES");
+  else puts("NO");
+  
+  return 0;
+}
+*/
