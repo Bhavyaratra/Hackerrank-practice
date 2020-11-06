d3cad3821150cce0cda2c6ee146529f24b49f1f0
Bhavya Ratra <bhavya.r1220@gmail.com>
2020-11-06 17:33:06 +0530

    added anagram.cpp

---
+++ Algorithm/Strings/anagram.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include<string>
+
+using namespace std;
+
+int main()
+{ 
+  int t;
+  cin>>t;
+  while(t--)
+   {
+    string s;
+    cin>>s;
+    int c=0;
+
+    int len=s.size();
+    int arr[26]={0};
+    if(len%2!=0)
+    {
+        cout<<"-1"<<endl;
+    }
+    else
+    {
+        for(int i=0;i<(len/2)-1;i++)
+        {
+            arr[s[i]-'a']++;
+        }
+
+        for(int i=0;i<(len/2)-1;i++)
+        {
+            arr[s[i]-'a']--;
+        }
+
+        for(int x : arr)
+        {
+            if(x!=0)
+            {
+                c=c+abs(x);
+            }
+        }
+     cout<<c<<endl;
+    }
+    
+ 
+     
+}
+}
