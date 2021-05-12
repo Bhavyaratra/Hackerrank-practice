f561033bfc138c4a5e3519442303cc64b5979934
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-05-12 13:24:59 +0530

    added class

--- Datastructure/Tree/chkBST.cpp
+++ Datastructure/Tree/chkBST.cpp
@@ -1,14 +1,17 @@
 /* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.  
 
 The Node struct is defined as follows:
-	struct Node {
+	*/
+#include<bits/stdc++.h>
+using namespace std;
+    struct Node {
 		int data;
 		Node* left;
 		Node* right;
-	}
+	};
     
-*/
-       
+
+class Solution{
 vector<int> v;
 	bool checkBST(Node* root) {
 		//inorder traversal of bst gives keys in ascending order 
@@ -29,3 +32,4 @@ vector<int> v;
             
         return true;
 	}
+};
